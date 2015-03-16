9ba3f62acf87594b743a6a060c02758a3bed9065
Ajay Modi <mitajakom@gmail.com>
2015-03-16 16:36:49 -0700

    Adding MBR functions

---
+++ Task2_sinatra/app/models/Hasher.rb
@@ -0,0 +1,40 @@
+require 'digest/md5'
+require 'digest/sha1'
+
+$BUFLEN = 1024
+
+class Hasher
+	
+	def initialize(method, filepath)
+		if (method.upcase == "-SHA1")
+			@hashfunc = Digest::SHA1.new
+			@hashname = "SHA1"
+		else
+			@hashfunc = Digest::MD5.new
+			@hashname = "MD5"
+		end
+		@fullfilename = filepath
+	end
+
+	def hashname
+		@hashname
+	end
+
+	# Compute hash code
+	def hashsum
+		open(@fullfilename, "r") do |io|
+			puts "Reading "+@fullfilename
+			counter = 0
+			while (!io.eof)
+				readBuf = io.readpartial($BUFLEN)
+				putc '.' if ((counter+=1) % 3 == 0)
+				@hashfunc.update(readBuf)
+			end
+		end
+		return @hashfunc.hexdigest
+	end
+end
+
+def usage
+	puts "Usage: Hasher.rb [-SHA1|-MD5] filename"
+end
--- Task2_sinatra/app/routes/main.rb
+++ Task2_sinatra/app/routes/main.rb
@@ -10,8 +10,67 @@ post "/add_files" do
   puts params
   
   if(File.exist?(params["filepath"]))
-  	a = { :fullpath => params["filepath"], :filename => File.basename(params["filepath"]), :size => File.size?(params["filepath"])   }.to_json
+  	{ :fullpath => params["filepath"], :filename => filename(params["filepath"]), :size => filesize(params["filepath"]) }.to_json
   else
   	{ :error => "file does not exist"}.to_json
   end
+end
+
+post "/calculate_files" do
+  puts params
+  
+  if(File.exist?(params["filepath"]))
+	hashcomp1 = Hasher.new("-SHA1", params["filepath"])
+	hashcomp2 = Hasher.new("-MD5", params["filepath"])
+	
+	File.open("data/MD5-"+filename(params["filepath"])+".txt", "w") do |value|
+		value.write(hashcomp2.hashsum)
+	end
+
+	File.open("data/SHA1-"+filename(params["filepath"])+".txt", "w") do |value|
+		value.write(hashcomp1.hashsum)
+	end
+
+	mbr = getMBR(params["filepath"])
+	partition_1 = getPartition1(params["filepath"])
+	partition_2 = getPartition2(params["filepath"])
+	partition_3 = getPartition3(params["filepath"])
+	partition_4 = getPartition4(params["filepath"])
+
+	{ :filename => filename(params["filepath"]),:SHA1 => hashcomp1.hashsum, :MD5 => hashcomp2.hashsum,  }.to_json
+  else
+  	{ :error => "file does not exist"}.to_json
+  end
+end
+
+def filesize(name)
+	File.size?(name)
+end
+
+def filename(name)
+	File.basename(name)
+end
+
+# def printresult(filename, method, sum)
+# 	puts "\n" + filename + " ==> "+ method + ": " + sum	
+# end
+
+def getMBR filename
+	`hexdump -e '16/1 "%02x " "\n"' -n 512 #{filename}`
+end
+
+def getPartition1(filename)
+	`hexdump -e '16/1 "%02x " "\n"' -n 16 -446 #{filename}`
+end
+
+def getPartition2(filename)
+	`hexdump -e '16/1 "%02x " "\n"' -n 16 -462 #{filename}`
+end
+
+def getPartition3(filename)
+	`hexdump -e '16/1 "%02x " "\n"' -n 16 -478 #{filename}`
+end
+
+def getPartition4(filename)
+	`hexdump -e '16/1 "%02x " "\n"' -n 16 -494 #{filename}`
 end
