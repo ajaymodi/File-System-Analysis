c8ca7ed0e4b074253bf86b8a6eb891fb754fcc19
Ajay Modi <mitajakom@gmail.com>
2015-03-24 02:51:47 -0700

    adding calculation for task 2 part 2 for FAT 16/32

--- Task2_sinatra/app/models/Partition.rb
+++ Task2_sinatra/app/models/Partition.rb
@@ -39,22 +39,52 @@ class Partition
 		'('+key+')'+@partition[key]
 	end
 
+	def get_reserved_area(detail)
+		hex = convert_to_little_endian(detail[42..47])
+		ra = hex.to_i(16)
+	end
+
+	def get_sectors_per_cluster(detail)
+		spc = detail[39..40].to_i(16)
+	end
+
+	def get_number_of_fat(detail)
+		spc = detail[48..49].to_i(16)
+	end
+
+	def get_size_of_fat(detail,file_system)
+		size = 0
+		if(file_system=="06")
+			hex = convert_to_little_endian(detail[66..70])
+			size = hex.to_i(16)	
+		else
+			hex = convert_to_little_endian(detail[108..118])
+			size = hex.to_i(16)
+		end	
+		return size
+	end
+
+	def get_root_directory(detail)
+		hex = convert_to_little_endian(detail[51..55])
+		size = hex.to_i(16)*32/512
+	end
 
 	def get_partition_address(detail)
-		hex = convert_to_little_endian_address(detail)
+		hex = convert_to_little_endian(detail[24..34])
 		address = hex.to_i(16)
 	end
 
 	def get_partition_size(detail)
-		hex = convert_to_little_endian_size(detail)
+		hex = convert_to_little_endian(detail[36..46])
 		size = hex.to_i(16)
 	end
 
-	def convert_to_little_endian_address(arg)
-    return arg[33..34]+arg[30..31]+arg[27..28]+arg[24..25]
-  end
-
-  def convert_to_little_endian_size(arg)
-    return arg[45..46]+arg[42..43]+arg[39..40]+arg[36..37]
+	def convert_to_little_endian(arg)
+		arg = arg.gsub(/\s+/, "")  unless arg.nil?
+		if(arg.length==8)
+			return arg.scan(/(..)(..)(..)(..)/).map(&:reverse).join
+		else
+			return arg.scan(/(..)(..)/).map(&:reverse).join
+		end
   end
 end
--- Task2_sinatra/app/routes/main.rb
+++ Task2_sinatra/app/routes/main.rb
@@ -36,8 +36,12 @@ post "/calculate_files" do
 
 	mbr = getMBR(params["filepath"])
 	partition = get_partition_details(params["filepath"])
-	
-	{ :filename => filename(params["filepath"]),:SHA1 => sha1, :MD5 => md5, :partition => partition }.to_json
+	partition.each do |k,v|
+		if(partition[k]["type"][1..2]=="06" || partition[k]["type"][1..2]=="0B")
+			partition = calculate_details_for_partition(params["filepath"],partition,k,partition[k]["type"][1..2])
+		end
+	end
+	{ :filename => filename(params["filepath"]),:SHA1 => sha1, :MD5 => md5, :partition => partition}.to_json
   else
   	{ :error => "file does not exist"}.to_json
   end
@@ -55,30 +59,17 @@ def getMBR filename
 	`hexdump -e '16/1 "%02x " "\n"' -n 512 #{filename}`
 end
 
-def getPartition1(filename)
-	`hexdump -e '16/1 "%02x " "\n"' -n 16 -s 446 #{filename}`
-end
-
-def getPartition2(filename)
-	`hexdump -e '16/1 "%02x " "\n"' -n 16 -s 462 #{filename}`
-end 
-
-def getPartition3(filename)
-	`hexdump -e '16/1 "%02x " "\n"' -n 16 -s 478 #{filename}`
-end
-
-def getPartition4(filename)
-	`hexdump -e '16/1 "%02x " "\n"' -n 16 -s 494 #{filename}`
+def getbytes(filename, offset, no_of_bytes)
+	`hexdump -e '#{no_of_bytes}/1 "%02x " "\n"' -n #{no_of_bytes} -s #{offset} #{filename}`
 end
 
 def get_partition_details(filepath)
-	partition_1 = getPartition1(filepath)
-	partition_2 = getPartition2(filepath)
-	partition_3 = getPartition3(filepath)
-	partition_4 = getPartition4(filepath)
+	partition_1 = getbytes(filepath, 446, 16)
+	partition_2 = getbytes(filepath, 462, 16)
+	partition_3 = getbytes(filepath, 478, 16)
+	partition_4 = getbytes(filepath, 494, 16)
 	h = {"partition1" => {},"partition2" => {},"partition3" => {},"partition4" => {}}
 	p = Partition.new
-
 	h["partition1"]["type"] = p.get_partition_type(partition_1)
 	h["partition2"]["type"] = p.get_partition_type(partition_2)
 	h["partition3"]["type"] = p.get_partition_type(partition_3)
@@ -94,5 +85,21 @@ def get_partition_details(filepath)
 	h["partition3"]["size"] = p.get_partition_size(partition_3)
 	h["partition4"]["size"] = p.get_partition_size(partition_4)
 
+	h["partition1"]["details"] = {}
+	h["partition2"]["details"] = {}
+	h["partition3"]["details"] = {}
+	h["partition4"]["details"] = {}
+
 	return h
+end
+
+def calculate_details_for_partition(filepath,hash,partition_number,partition_type)
+	vbr = getbytes(filepath,hash[partition_number]["starting_address"]*512, 512)
+	p = Partition.new
+	hash[partition_number]["details"]["reserved_area"] = p.get_reserved_area(vbr)
+	hash[partition_number]["details"]["sectors_per_cluster"] = p.get_sectors_per_cluster(vbr)
+	hash[partition_number]["details"]["no_of_fat"] = p.get_number_of_fat(vbr)
+	hash[partition_number]["details"]["size_of_fat"] = p.get_size_of_fat(vbr,partition_type)
+	hash[partition_number]["details"]["root_directory"] = p.get_root_directory(vbr)
+	return hash
 end
