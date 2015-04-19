3ecb88ea960e813a10384b5674a6d14362c0fb96
Ajay Modi <mitajakom@gmail.com>
2015-04-18 23:33:11 -0700

    fixing help for commands

--- Task1/address4forensics/lib/address4forensics/application.rb
+++ Task1/address4forensics/lib/address4forensics/application.rb
@@ -63,78 +63,63 @@ module Address4Forensics
         opts.banner = "Usage: address4forensics [options]"
         answer = -1
     
-        opts.on("-L", "--logical", "Calculate the logical address",
-          "from either the cluster address or the physical address.",
+        opts.on("-L", "--logical", "Calculate the logical address from either the cluster address or the physical address.",
           "Either -c or -p must be given.") { params[:calculation] = "calculate_logical_address" }
         
-        opts.on("-P", '--physical', 'Calculate the physical ','
-          address from either the cluster address or the logical address. ','
-          Either -c or -l must be given.') { params[:calculation] = "calculate_physical_address"  }
+        opts.on("-P", "--physical", "Calculate the physical address from either the cluster address or the logical address. ",
+          "Either -c or -l must be given.") { params[:calculation] = "calculate_physical_address"  }
         
-        opts.on("-C", '--cluster', 'Calculate the cluster address ','
-          from either the logical address or the physical address. ','
-          Either -l or -p must be given.') { params[:calculation] = "calculate_cluster_address"  }
+        opts.on("-C", "--cluster", "Calculate the cluster address from either the logical address or the physical address. ",
+          "Either -l or -p must be given.") { params[:calculation] = "calculate_cluster_address"  }
         
-        opts.on("-b", "--partition-start offset", Integer , 'This specifies the ','
-          physical address (sector number) of the start of the partition, and ','
-          defaults to 0 for ease in working with images of a single partition. ','
-          The offset vaulue will always translate into logical address 0.') do |offset|
+        opts.on("-b", "--partition-start offset", Integer , "This specifies the physical address (sector number) of the start of the partition, ",
+          "and defaults to 0 for ease in working with images of a single partition. ",
+          "The offset vaulue will always translate into logical address 0.") do |offset|
           params[:partition_start_offset] = offset.to_i
         end
         
-        opts.on("-B", "--byte-address", 'Instead of returning ','
-          sector values for the conversion, this returns the byte address of ','
-          the calculated value, which is the number of sectors multiplied by ','
-          the number of bytes per sector.') { params[:byte_address]   = true }
+        opts.on("-B", "--byte-address", "Instead of returning sector values for the conversion,",
+          "this returns the byte address of the calculated value, which is the number of sectors multiplied by ",
+          "the number of bytes per sector.") { params[:byte_address]   = true }
         
-        opts.on("-s", "--sector-size bytes", Integer, 'When the -B option is used, ','
-          this allows for a specification of bytes per sector other than the ','
-          default 512. Has no affect on output without -B.') do |bytes|
+        opts.on("-s", "--sector-size bytes", Integer, "When the -B option is used, ",
+          "this allows for a specification of bytes per sector other than the ",
+          "default 512. Has no affect on output without -B.") do |bytes|
           params[:sector_size]   = bytes.to_i 
         end
         
-        opts.on("-l", "--logical-known address", Integer, 'This specifies the ','
-          known logical address for calculating either a cluster address or a ','
-          physical address. When used with the -L option, this simply ','
-          returns the value given for address.') do |address|
+        opts.on("-l", "--logical-known address", Integer, "This specifies the known logical address for calculating either a cluster address or a ",
+          "physical address. When used with the -L option, this simply returns the value given for address.") do |address|
           params[:logical_address]   = address.to_i 
           params[:logical_known_address]   = true
         end
         
-        opts.on("-p", "--physical-known address", Integer, 'This specifies the ','
-          known physical address for calculating either a cluster address or a ','
-          logical address. When used with the -P option, this simply ','
-          returns the value given for address.') do |address|
+        opts.on("-p", "--physical-known address", Integer, "This specifies the known physical address for calculating either a cluster address or a ",
+          "logical address. When used with the -P option, this simply returns the value given for address.") do |address|
           params[:physical_address]   = address.to_i
           params[:physical_known_address]   = true
         end
         
-        opts.on("-c", "--cluster-known address", Integer, 'This specifies the ','
-          known cluster address for calculating either a logical address or a ','
-          physical address. When used with the -C option, this simply ','
-          returns the value given for address. Note that options -k, ','
-          -r, -t, and -f must be provided with this option.') do |address|
+        opts.on("-c", "--cluster-known address", Integer, "This specifies the known cluster address for calculating either a logical address or a ",
+          "physical address. When used with the -C option, this simply returns the value given for address.",
+           "Note that options -k, -r, -t, and -f must be provided with this option.") do |address|
           params[:cluster_address]   = address.to_i
           params[:cluster_known_address]   = true
         end
         
-        opts.on("-k", "--cluster-size sectors", Integer, 'This specifies the ','
-          number of sector per cluster.') do |sectors|
+        opts.on("-k", "--cluster-size sectors", Integer, "This specifies the number of sector per cluster.") do |sectors|
           params[:cluster_size]   = sectors.to_i
         end
         
-        opts.on("-r", "--reserved sectors", Integer, 'This specifies the ','
-          number of reserved sectors in the partition.') do |sectors|
+        opts.on("-r", "--reserved sectors", Integer, "This specifies the number of reserved sectors in the partition.") do |sectors|
           params[:reserved_sectors]   = sectors.to_i 
         end
         
-        opts.on("-t", "--fat_tables tables", Integer, 'This specifies the ','
-          number of FAT tables, which is usually 2.') do |tables|
+        opts.on("-t", "--fat_tables tables", Integer, "This specifies the number of FAT tables, which is usually 2.") do |tables|
           params[:fat_tables]   = tables.to_i
         end
         
-        opts.on("-f", "--fat_length sectors", Integer, 'This specifies the ','
-          length of each FAT table in sectors.') do |sectors|
+        opts.on("-f", "--fat_length sectors", Integer, "This specifies the length of each FAT table in sectors.") do |sectors|
           params[:fat_length]   = sectors.to_i
         end
         
